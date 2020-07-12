ad44a1bdbeaf0244405d034aa7311f5f4ac84bb3
AndrewPolukhin <qq7039108@gmail.com>
2020-07-12 18:38:46 +0300

    Made more out-of-the-box functionality by putting everything in the __init__ module

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open("README.md", 'r') as file:
 setup(
     name="JSONManipulator",
     description="A Python package to manipulate objects in JSON files.",
-    version="1.0",
+    version="1.1",
     long_description=long_description,
     long_description_content_type='text/markdown',
     author="Andrew Polukhin",
     author_email="andrewmathematics2003@gmail.com",
     license="MIT",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Stable",
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English'
